a7637a05e646e9936750c61b895cd91b4cd724da
Nabil Houidi <138534428+NH-2@users.noreply.github.com>
2024-05-21 20:28:49 +0100

    initial commit: gin app, dockerfile & k8s files

---
+++ src/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World",
+		})
+	})
+	r.GET("/healthz", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"Healthy": true,
+		})
+	})
+	r.GET("/healthz/ready", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"Ready": true,
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
