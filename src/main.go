48a8fe2771fa94f10c895b7640abd3bcbea35f6f
Nabil Houidi <138534428+NH-2@users.noreply.github.com>
2024-05-21 20:28:49 +0100

    feat: Add artificial load to go app

--- src/main.go
+++ src/main.go
@@ -1,11 +1,17 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"runtime"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
+		generate_load() // artificial load to simulate request processing
 		c.JSON(200, gin.H{
 			"message": "Hello World",
 		})
@@ -21,4 +27,21 @@ func main() {
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
+}
+
+func generate_load() {
+	// Number of logical CPUs on the local machine
+	cores := runtime.NumCPU()
+
+	// Generate load on each of the cores
+	for i := 0; i < cores; i++ {
+		go func() {
+			for {
+				// The tight loop generates the Load like crazy!
+			}
+		}()
+	}
+
+	// Let it run for a certain amount of time, e.g., 10 milli-seconds.
+	time.Sleep(10 * time.Millisecond)
 }
